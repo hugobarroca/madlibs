eb5a4bafe908394509a77d5011097e0d5722b480
hugobarroca98@gmail.com <xCLVswfGlcerzvhxhNEkyGdmLGftThgAoxwKqtfbObrvgqecHZ>
2020-12-25 22:11:38 +0000

    Added primitive interaction with the madlibz API.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import requests
+import pandas as pd
 from blanks_processor import BlanksProcessor
 from input_parser import InputParser
 
 if __name__ == '__main__':
+    response = requests.get("http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=25").json()
+    print(response)
     ip = InputParser()
     ip.get_input()
     bp = BlanksProcessor(ip.phrase_with_blanks, ip.words_to_replace)
