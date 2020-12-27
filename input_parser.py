1500298595afefe36bb3dd819db8504e167c9016
Hugo Barroca <hugobarroca98@gmail.com>
2020-12-27 13:10:40 +0000

    Removed most of the functionality from main, placed it in the InputParser class.

--- input_parser.py
+++ input_parser.py
@@ -1,17 +1,31 @@
-# Gets a valid string from the user.
+import requests
+
 class InputParser:
     def __init__(self):
-        self.phrase_with_blanks = ''
+        self.phrases = []
         self.words_to_replace = []
         self.number_of_blanks = 0
 
     def get_input(self):
-        self.get_phrase_with_blanks()
-        self.get_words_to_replace()
+        user_input = input("Enter 1 to get a randomly generated phrase from the API, or 2 to enter a phrase yourself:\n")
+        if user_input == "1":
+            self.get_input_from_the_api()
+        else:
+            self.get_phrase_with_blanks()
+            self.get_words_to_replace()
+
+    def get_input_from_the_api(self):
+        response = requests.get("http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=13").json()
+        print(response)
+        list_of_phrases = response["value"]
+        list_of_blanks = response["blanks"]
+        nr_phrases = len(list_of_phrases)
+        nr_blanks = len(list_of_blanks)
+        if nr_phrases == nr_blanks:
 
     def get_phrase_with_blanks(self):
         while not self.input_is_valid():
-            self.phrase_with_blanks = input('Please write a phrase with underscores to represent blanks:\n')
+            phrase_with_blanks = input('Please write a phrase with underscores to represent blanks:\n')
         self.number_of_blanks = self.phrase_with_blanks.count("_")
 
     def get_words_to_replace(self):
--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-import requests
-import json
-import pandas as pd
-from blanks_processor import BlanksProcessor
-from input_parser import InputParser
-
-#Test
-def json_print(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
 
 
 if __name__ == '__main__':
-    response = requests.get("http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=25").json()
-    json_print(response)
+
 
     # ip = InputParser()
     # ip.get_input()
