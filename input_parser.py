dad70adccbef86149969ee317f8bfb42827866e5
Hugo Barroca <hugobarroca98@gmail.com>
2020-12-27 14:21:28 +0000

    Reworked the way InputParser works.

--- input_parser.py
+++ input_parser.py
@@ -1,18 +1,24 @@
 import requests
+import sys
+
+
+# This class queries the user for input, and processes it in such a way that it ends up storing a list of phrases, and a
+# list of words to fill in between those phrases.
 
 class InputParser:
     def __init__(self):
         self.phrases = []
         self.words_to_replace = []
-        self.number_of_blanks = 0
 
     def get_input(self):
-        user_input = input("Enter 1 to get a randomly generated phrase from the API, or 2 to enter a phrase yourself:\n")
+        user_input = input("Enter 1 to get a randomly generated phrase from the API,"
+                           " or 2 to enter a phrase yourself:\n")
         if user_input == "1":
             self.get_input_from_the_api()
+        elif user_input == "2":
+            self.get_input_from_the_user()
         else:
-            self.get_phrase_with_blanks()
-            self.get_words_to_replace()
+            sys.exit()
 
     def get_input_from_the_api(self):
         response = requests.get("http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=13").json()
@@ -22,28 +28,21 @@ class InputParser:
         nr_phrases = len(list_of_phrases)
         nr_blanks = len(list_of_blanks)
         if nr_phrases == nr_blanks:
+            pass
 
-    def get_phrase_with_blanks(self):
-        while not self.input_is_valid():
-            phrase_with_blanks = input('Please write a phrase with underscores to represent blanks:\n')
-        self.number_of_blanks = self.phrase_with_blanks.count("_")
+    def get_input_from_the_user(self):
+        self.fill_list_of_phrases()
+        self.fill_words_to_replace()
 
-    def get_words_to_replace(self):
-        for x in range(0, self.number_of_blanks):
+    def fill_list_of_phrases(self):
+        phrase_with_underscores = ''
+        while phrase_with_underscores == '':
+            phrase_with_underscores = input('Please write a phrase with underscores to represent blanks:\n')
+        self.phrases = phrase_with_underscores.split('_')
+
+    def fill_words_to_replace(self):
+        self.words_to_replace = []
+        number_of_blanks = len(self.phrases) - 1
+        for x in range(0, number_of_blanks):
             new_word = input("Please input word to replace:\n")
             self.words_to_replace.append(new_word)
-
-    def input_is_valid(self):
-        if '_' not in self.phrase_with_blanks:
-            return False
-        if self.phrase_with_blanks == '':
-            return False
-        return True
-
-
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-
+from blanks_processor import BlanksProcessor
+from input_parser import InputParser
 
 if __name__ == '__main__':
-
-
-    # ip = InputParser()
-    # ip.get_input()
-    # bp = BlanksProcessor(ip.phrase_with_blanks, ip.words_to_replace)
-    # print(bp.get_filled_string())
+    ip = InputParser()
+    ip.get_input()
+    bp = BlanksProcessor(ip.phrases, ip.words_to_replace)
+    print(bp.get_filled_string())
